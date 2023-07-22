8f7cfd87cc37f76df1f279bed9d30f81d8e3ac3d
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-07-23 00:35:46 +0530

    compact ui an cleanup

--- src/components/DownloaderInfo.tsx
+++ src/components/DownloaderInfo.tsx
@@ -2,12 +2,14 @@ import { Flex, TextInput, LoadingOverlay, Box } from "@mantine/core";
 import type { GithubRepo, ResolvedRepoData } from "../lib/constants";
 
 // icons
-import { AiOutlineCloudDownload, AiFillLock, AiFillFolderOpen } from "react-icons/ai";
+import {
+  AiOutlineCloudDownload,
+  AiFillLock,
+  AiFillFolderOpen,
+} from "react-icons/ai";
 import { GoRepo, GoGitBranch } from "react-icons/go";
 
-
-import prettyBytes from 'pretty-bytes';
-
+import prettyBytes from "pretty-bytes";
 
 /**
  *  Used to show the information at the top of the page
@@ -20,45 +22,52 @@ export default function DownloaderInfoComponent({
   resolvedData,
   githubData,
   files,
-  loading
-}: {resolvedData: ResolvedRepoData | null; githubData: GithubRepo | null; loading: boolean; files: { count: number, size: number }}) {
+  loading,
+}: {
+  resolvedData: ResolvedRepoData | null;
+  githubData: GithubRepo | null;
+  loading: boolean;
+  files: { count: number; size: number };
+}) {
   return (
     <Box pos="relative">
-      <LoadingOverlay  loaderProps={{ size: 'lg', color: 'blue', variant: 'bars' }} visible={loading} overlayBlur={2} />
-      <Flex direction="column" gap="sm">
-      <span>
+      <LoadingOverlay
+        loaderProps={{ size: "lg", color: "blue", variant: "bars" }}
+        visible={loading}
+        overlayBlur={2}
+      />
+      <Flex direction="column" gap="xs">
         <TextInput
           placeholder="Loading..."
-          value={githubData?.full_name || 'Invalid Repo'}
+          value={githubData?.full_name || "Invalid Repo"}
           readOnly
+          size="sm"
           icon={githubData?.private ? <AiFillLock /> : <GoRepo />}
         />
-      </span>
-      <span>
         <TextInput
           placeholder="Loading..."
           icon={<GoGitBranch />}
-          value={resolvedData?.branch || 'Invalid branch'}
+          value={resolvedData?.branch || "Invalid branch"}
+          size="sm"
           readOnly
         />
-      </span>
-      <span>
         <TextInput
           placeholder="Loading..."
           icon={<AiFillFolderOpen />}
-          value={resolvedData?.directory || 'Invalid Directory'}
+          value={resolvedData?.directory || "Invalid Directory"}
+          size="sm"
           readOnly
         />
-      </span>
-      <span>
         <TextInput
           placeholder=""
           withAsterisk
-          value={`Downloading ${files.count} File(s) of total size ${prettyBytes(files.size)}`}
+          value={`Downloading ${
+            files.count
+          } File(s) of total size ${prettyBytes(files.size)}`}
           readOnly
           icon={<AiOutlineCloudDownload />}
+          size="sm"
         />
-      </span>
       </Flex>
     </Box>
   );
